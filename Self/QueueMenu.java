a8460a2489af95cfa39049868336a72aec91557f
Samia Naz <nsamia312@gmail.com>
2026-05-14 21:29:38 +0500

    Continue queue practice with enqueue implementation

---
+++ Self/QueueMenu.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class QueueMenu {
+    static Scanner sc = new Scanner(System.in);
+    static int[] queue = new int[4];
+    static int front = -1;
+    static int rear = -1;
+
+    //Enqueue()
+    static void enqueue(int data)
+    {
+        if(rear == queue.length-1)
+        {
+            System.out.println("Queue Overflow!");
+            return;
+        }
+       else
+       {
+         if(front == -1)
+        {
+            front = 0;
+        }  
+        rear++;
+        queue[rear] = data;
+       }
+        
+    } 
+
+    public static void main(String[] args) {
+        
+    }
+}
