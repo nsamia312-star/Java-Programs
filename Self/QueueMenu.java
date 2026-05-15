f5456bf82465c1446f1ca7a4db6adabaad0d5b60
Samia Naz <nsamia312@gmail.com>
2026-05-15 23:47:44 +0500

    Update basic queue program with dequeue logic

--- Self/QueueMenu.java
+++ Self/QueueMenu.java
@@ -23,8 +23,23 @@ public class QueueMenu {
         rear++;
         queue[rear] = data;
        }
+
+       
         
     } 
+    
+ // Dequeue Method
+    static int dequeue() {
+        if (front == -1 || front > rear) {
+            System.out.println("Queue underflow!");
+            return 0;
+        } else {
+            int value = queue[front];
+            front++;
+            return value;
+        }
+    }
+
 
     public static void main(String[] args) {
         
